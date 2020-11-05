63c5c772b7181451b24740687ead664095d106c7
Raghav Sethi <rsraghavsethi@gmail.com>
2020-11-05 17:48:57 +0530

    update

--- Unifynd-Test/ThirdViewController.swift
+++ Unifynd-Test/ThirdViewController.swift
@@ -8,13 +8,68 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
+    
+    var numbers:[Int] = []
 
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var allNumsLabel: UILabel!
+    @IBOutlet weak var secondLargestLabel: UILabel!
     @IBOutlet weak var enterButton: UIButton!
     @IBAction func enterButton(_ sender: Any) {
+        if textField.text != "" {
+            let n = Int(textField.text ?? "0")!
+            self.numbers.append(n)
+            print(numbers)
+            DispatchQueue.main.async {
+                self.textField.text = ""
+                self.view1.isHidden = false
+                self.view2.isHidden = false
+                var stringArray = self.numbers.map { String($0) }
+               
+
+                self.allNumsLabel.text = stringArray.joined(separator: "\n")
+                if self.numbers.count > 1   {
+                    let number = self.secondLargest(nums: self.numbers)
+                    self.secondLargestLabel.text = "\(number)"
+                }
+            }
+        }
     }
     @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.setupUI()
     }
+    
+    func setupUI()  {
+        self.view1.layer.cornerRadius = 10
+        self.view2.layer.cornerRadius = 10
+        self.view1.isHidden = true
+        self.view2.isHidden = true
+        
+    }
+    
+    func secondLargest(nums: [Int]) -> Int  {
+        var largest = 0
+        var secondLargest = 0
+        
+        for index in 0..<nums.count {
+            let val = nums[index]
+            
+            if val > largest {
+                secondLargest = largest
+                largest = val
+            }
+            else if val > secondLargest, val != largest {
+                secondLargest = val
+            }
+        }
+        if secondLargest == 0   {
+            print("Invalid")
+        }
+        
+        return secondLargest
+    }
+    
 }
